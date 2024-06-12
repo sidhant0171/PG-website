d54cd92959d4e764f190bc20e41c1a64d392b031
sidhant bhardwaj <142473258+sidhant0171@users.noreply.github.com>
2024-06-12 11:36:06 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
-<?php
+
+<?php  
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "";  
-$db_name = "";
-
+$db_name = "";   
 
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
